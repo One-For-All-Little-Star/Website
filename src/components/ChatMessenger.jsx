52233b3e11b00d6fb7896e7e8a441ab8ed97da0b
tuannguyen22 <tuannguyenit263@gmail.com>
2022-02-27 20:51:56 +0700

    Fix confict

--- src/components/ChatMessenger.jsx
+++ src/components/ChatMessenger.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import MessengerCustomerChat from "react-messenger-customer-chat";
 
 const ChatMessenger = (props) => {
-  return await(
+  return (
     <MessengerCustomerChat pageId="101660712332854" appId="4794169103992534" />
   );
 };
