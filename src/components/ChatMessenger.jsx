c25ab6557b32cf98db475a9faacaac39f009dcbb
hanguyen2911 <ha.nguyen22@student.passerellesnumeriques.org>
2022-02-27 19:51:27 +0700

    update chat with messenger

--- src/components/ChatMessenger.jsx
+++ src/components/ChatMessenger.jsx
@@ -1,14 +1,10 @@
 import React from "react";
 import MessengerCustomerChat from "react-messenger-customer-chat";
 
-function ChatMessenger(props) {
-  return (
-    <MessengerCustomerChat
-            pageId="101660712332854"
-
-            appId="4794169103992534"
-          />
+const ChatMessenger = (props) => {
+  return await(
+    <MessengerCustomerChat pageId="101660712332854" appId="4794169103992534" />
   );
-}
+};
 
 export default ChatMessenger;
